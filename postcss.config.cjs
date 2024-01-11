ca02c65cb712b151eecf4c9e7192c90a74ac28db
justinrbeattie <justin.r.beattie@gmail.com>
2024-01-11 15:49:08 +1100

    bootstrap

---
+++ postcss.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    "postcss-preset-env": {
+      stage: 3,
+      features: {
+        "nesting-rules": true,
+      },
+    },
+    "css-has-pseudo": {},
+    "@csstools/postcss-oklab-function":{}
+  },
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { builderDevTools } from "@builder.io/dev-tools/vite";
 
 export default defineConfig((): UserConfig => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [builderDevTools(), qwikCity(), qwikVite(), tsconfigPaths()],
     server: {
       headers: {
         "Cache-Control": "public, max-age=0",
