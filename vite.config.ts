3053f022273f17b7d42255fc035489b1f634d500
justinrbeattie <justin.r.beattie@gmail.com>
2024-01-11 15:50:13 +1100

    bootstrap

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,18 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { builderDevTools } from "@builder.io/dev-tools/vite";
+import { partytownVite } from "@builder.io/partytown/utils";
+import { join } from "path";
 
 export default defineConfig((): UserConfig => {
   return {
-    plugins: [builderDevTools(), qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [
+      builderDevTools(),
+      qwikCity(),
+      qwikVite(),
+      tsconfigPaths(),
+      partytownVite({ dest: join(__dirname, "dist", "~partytown") }),
+    ],
     server: {
       headers: {
         "Cache-Control": "public, max-age=0",
