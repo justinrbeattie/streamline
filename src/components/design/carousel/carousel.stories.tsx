ca503fb968ec61f168000eb8bb761c8f76f50b99
Justin Beattie <jrb@fiftydegreesnorth.com>
2024-01-26 17:50:40 +1100

    carousel complete

--- src/components/design/carousel/carousel.stories.tsx
+++ src/components/design/carousel/carousel.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from "storybook-framework-qwik";
-import { CarouselComponent, type CarouselProps, CarouselItemComponent } from "./carousel";
-
+import { CarouselComponent, type CarouselProps } from "./carousel";
 
 const meta: Meta<CarouselProps> = {
   component: CarouselComponent,
@@ -12,21 +11,54 @@ export default meta;
 
 export const Carousel: Story = {
   args: {
-
+    id: "carousel1",
+    ariaLabel: "Aria Label for Carousel 1",
+    ariaRoleDescription: "Description for Carousel 1",
+    showButtons: true,
+    showNavigation: true,
+    carouselItemList: [
+      {
+        ariaLabel: "Item 1",
+        ariaRoleDescription: "Description for Item 1",
+        ariaHidden: "false",
+        ariaCurrent: "false",
+      },
+      {
+        ariaLabel: "Item 2",
+        ariaRoleDescription: "Description for Item 2",
+        ariaHidden: "false",
+        ariaCurrent: "false",
+      },
+      {
+        ariaLabel: "Item 3",
+        ariaRoleDescription: "Description for Item 3",
+        ariaHidden: "false",
+        ariaCurrent: "false",
+      },
+      {
+        ariaLabel: "Item 4",
+        ariaRoleDescription: "Description for Item 4",
+        ariaHidden: "false",
+        ariaCurrent: "false",
+      },
+      {
+        ariaLabel: "Item 5",
+        ariaRoleDescription: "Description for Item 5",
+        ariaHidden: "false",
+        ariaCurrent: "false",
+      },
+      {
+        ariaLabel: "Item 6",
+        ariaRoleDescription: "Description for Item 6",
+        ariaHidden: "false",
+        ariaCurrent: "false",
+      },
+    ],
+    scrolledToStart: false,
+    scrolledToEnd: false,
+    carouselItemMinWidth: '28dvw',
+    carouselItemMinHeight: '50dvh',
   },
-  argTypes: {
-  },
-  render: (props) => 
-
-  <CarouselComponent {...props}>
-    <CarouselItemComponent> 1 </CarouselItemComponent>
-    <CarouselItemComponent> 2 </CarouselItemComponent>
-    <CarouselItemComponent> 3 </CarouselItemComponent>
-    <CarouselItemComponent> 4 </CarouselItemComponent>
-    <CarouselItemComponent> 5 </CarouselItemComponent>
-
-  </CarouselComponent>    
+  argTypes: {},
+  render: (props) => <CarouselComponent {...props}></CarouselComponent>,
 };
-
-
-
--- src/components/design/carousel/carousel.tsx
+++ src/components/design/carousel/carousel.tsx
@@ -1,223 +1,236 @@
-
-import { component$, useOnDocument, useSignal, useStore, useStylesScoped$, useVisibleTask$, $, Slot } from "@builder.io/qwik";
+import type { QRL } from "@builder.io/qwik";
+import {
+  component$,
+  useSignal,
+  useStylesScoped$,
+  Slot,
+  useVisibleTask$,
+  $,
+  useStore,
+} from "@builder.io/qwik";
 import styles from "./carousel.css?inline";
 import { IconComponent } from "../icon/icon";
-const intersectionObserverList: IntersectionObserver[] = []
-let mutationObserver: MutationObserver | undefined = undefined;
+
 export interface CarouselProps {
-
-
+  id: string;
+  ariaLabel: string;
+  ariaRoleDescription: string;
+  showButtons: boolean;
+  showNavigation: boolean;
+  carouselItemList: CarouselItem[];
+  scrolledToStart: boolean;
+  scrolledToEnd: boolean;
+  carouselItemMinWidth: string;
+  carouselItemMinHeight: string;
 }
-export const CarouselComponent = component$<CarouselProps>(() => {
-  const store: CarouselStore = useStore({
-    'aria-label': '',
-    showTabNavigation: true,
-    scrolledToStart: true,
-    scrolledToEnd: false,
-    totalItems: 0,
-    carouselItemList: []
-  });
-  const ul = useSignal<Element>();
+
+export interface CarouselItem {
+  ariaLabel: string;
+  ariaRoleDescription: string;
+  ariaHidden: string;
+  ariaCurrent: string;
+}
+
+export const CarouselComponent = component$<CarouselProps>((props) => {
   useStylesScoped$(styles);
-
-  useVisibleTask$(() => {
-    mutationObserverInit(ul.value as HTMLElement, store);
-    return () => mutationObserver?.disconnect();
-  });
-
-  useOnDocument(
-    'load',
-    $(() => {
-      if (!mutationObserver) {
-        mutationObserverInit(ul.value as HTMLElement, store);
+  const ul = useSignal<Element>();
+  const ulElement = ul.value;
+  const store = useStore(structuredClone({ ...props }));
+  const scrollPosition = $(() => {
+    if (ulElement) {
+      store.scrolledToStart = ulElement.scrollLeft === 0;
+      if (ulElement.scrollLeft > 0) {
+        const totalItemsWidth = Array.from(ulElement.children).reduce(
+          (acc, item) => {
+            return acc + item.clientWidth;
+          },
+          0
+        );
+        store.scrolledToEnd =
+          ulElement.scrollLeft + ulElement.clientWidth >= totalItemsWidth;
       }
-    })
-  );
+    }
+  });
 
   return (
-    <div class="wrapper">
-      <button
-        type="button"
-        title="Previous Item"
-        aria-label="Previous Item"
-        disabled={store.scrolledToStart}
-        onClick$={() => { navigateDirection('previous', ul.value); }}>
-        <IconComponent icon='InNavArrowLeft' size='small' variant="default" >
-        </IconComponent>
-      </button>
+    <div
+      class="wrapper"
+      scrolled-to-start={store.scrolledToStart || null}
+      scrolled-to-end={store.scrolledToEnd || null}
+      style={`--carousel-item-min-width:${store.carouselItemMinWidth}; --carousel-item-min-height:${store.carouselItemMinHeight};`}
+    >
+      {store.showButtons ? (
+        <button
+          type="button"
+          title="Previous Item"
+          aria-label="Previous Item"
+          disabled={store.scrolledToStart}
+          onClick$={() => {
+            navigateDirection("previous", ul.value);
+          }}
+        >
+          <IconComponent
+            icon="InNavArrowLeft"
+            size="small"
+            variant="default"
+          ></IconComponent>
+        </button>
+      ) : (
+        ""
+      )}
       <ul ref={ul}>
-        <Slot></Slot>
+        {store.carouselItemList.map((carouselItem, index) => (
+          //@ts-ignore
+          <li
+            key={`Slide ${index + 1} ${carouselItem.ariaLabel}`}
+            aria-roledescription={carouselItem.ariaRoleDescription}
+            aria-label={carouselItem.ariaLabel}
+            aria-hidden={carouselItem.ariaHidden}
+            aria-current={carouselItem.ariaCurrent}
+          >
+            <CarouselItemComponent
+              carouselItem={carouselItem}
+              scrollPosition={
+                index === 0 || index === store.carouselItemList.length - 1
+                  ? scrollPosition
+                  : undefined
+              }
+            >
+              <Slot></Slot>
+            </CarouselItemComponent>
+          </li>
+        ))}
       </ul>
-      <button
-        type="button"
-        title="Next Item"
-        aria-label="Next Item"
-        disabled={store.scrolledToEnd}
-        onClick$={() => { navigateDirection('next', ul.value); }}>
-        <IconComponent icon='InNavArrowRight' size='small' variant="default" >
-        </IconComponent>
-      </button>
+      {store.showButtons ? (
+        <button
+          type="button"
+          title="Next Item"
+          aria-label="Next Item"
+          disabled={store.scrolledToEnd}
+          onClick$={() => {
+            navigateDirection("next", ul.value);
+          }}
+        >
+          <IconComponent
+            icon="InNavArrowRight"
+            size="small"
+            variant="default"
+          ></IconComponent>
+        </button>
+      ) : (
+        ""
+      )}
 
-      {store.showTabNavigation ?
+      {store.showNavigation ? (
         <nav class="pagination" role="tablist" aria-multiselectable>
-          {store.carouselItemList.map((carouselItem) =>
+          {store.carouselItemList.map((carouselItem, index) => (
             <button
-              key={'tab-' + ((carouselItem?.index || 0) + 1)}
+              key={"tab-" + ((index || 0) + 1)}
               class="pagination-item"
               type="button"
               role="tab"
-              id={'tab-' + ((carouselItem?.index || 0) + 1)}
-              aria-controls={'tabpanel-' + ((carouselItem.index || 0) + 1)}
-              title={'Navigation Item ' + ((carouselItem.index || 0) + 1)}
-              aria-label='Navigation Item'
-              aria-setsize={store.totalItems}
-              aria-posinset={(carouselItem.index || 0) + 1}
-              onClick$={() => { navigateToIndex((carouselItem.index || 0), ul.value) }}
+              id={"tab-" + ((index || 0) + 1)}
+              aria-controls={"tabpanel-" + ((index || 0) + 1)}
+              title={
+                carouselItem.ariaLabel +
+                " Navigation Item " +
+                ((index || 0) + 1)
+              }
+              aria-label={
+                carouselItem.ariaLabel +
+                " Navigation Item " +
+                ((index || 0) + 1)
+              }
+              aria-setsize={store.carouselItemList.length}
+              aria-posinset={(index || 0) + 1}
+              aria-expanded={carouselItem.ariaCurrent}
+              onClick$={() => {
+                navigateToIndex(index || 0, ul.value);
+              }}
               /* @ts-ignore */
               tabindex="-1"
             ></button>
-          )}
-
+          ))}
         </nav>
-        : ''}
-
+      ) : (
+        ""
+      )}
     </div>
-
-
   );
 });
 
-
-export const CarouselItemComponent = component$(() => {
-  useStylesScoped$(`
-  li {
-    scroll-snap-align: start;
-    min-width:600px;
-    background:var(--surface-3);
-    padding:0;
-}
-  `);
-  return (
-    <li role="group" aria-roledescription="slide">
-      <Slot></Slot>
-    </li>
+const CarouselItemComponent = component$<{
+  carouselItem: CarouselItem;
+  scrollPosition?: QRL<() => void>;
+}>((props) => {
+  useStylesScoped$(
+    `div {
+      min-width:100%;
+      min-height: 100%;
+    }
+    `
   );
-});
+  const sentinel = useSignal<Element>();
+  // eslint-disable-next-line qwik/no-use-visible-task
+  useVisibleTask$(async (taskCtx) => {
+    if (sentinel.value) {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            props.carouselItem.ariaCurrent = (
+              entry.intersectionRatio === 1
+            ).toString();
+            props.carouselItem.ariaHidden = (
+              entry.intersectionRatio != 1
+            ).toString();
+            if (props.scrollPosition) {
+              props.scrollPosition();
+            }
+          });
+        },
+        {
+          threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+        }
+      );
 
+      observer.observe(sentinel.value);
 
-
-const mutationObserverInit = (element: HTMLElement, store: CarouselStore) => {
-
-  const config = { attributes: false, childList: true, subtree: true };
-  mutationObserver = new MutationObserver(() => mutationObserverCallback(element, store));
-  mutationObserver.observe(element, config);
-  mutationObserverCallback(element, store);
-}
-
-const mutationObserverCallback = (element: HTMLElement, store: CarouselStore) => {
-  store.totalItems = element.querySelectorAll('li').length;
-
-  intersectionObserverList.forEach((intersectionObserver) => {
-    intersectionObserver.disconnect()
-  });
-
-  store.carouselItemList = [];
-
-  for (let i = 0; i < element.children.length; i++) {
-
-    if (element.children[i].tagName === 'LI') {
-      store.carouselItemList.push({
-        index: i,
-        intersectionRatio: 0,
-        fullyVisible: false,
-        firstFullyVisible: false,
-        lastFullyVisible: false,
-        partiallyVisible: false,
-        notVisible: false,
-        scrollPercentage: 0,
-
+      taskCtx.cleanup(() => {
+        observer.disconnect();
       });
-      element.children[i].setAttribute('aria-label', `Slide ${i + 1} of ${store.totalItems}`);
-      element.children[i].setAttribute('id', `slide-${i + 1}`);
-      intersectionObserverInit(i, element.children[i] as HTMLElement, store);
     }
-
-  }
-};
-
-const intersectionObserverInit = (index: number, element: HTMLElement, store: CarouselStore) => {
-  const intersectionObserver = new IntersectionObserver(
-    (entries) => { intersectionObserverCallback(index, entries, store) },
-    {
-      root: this,
-      threshold: [0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1],
-    }
-  );
-  intersectionObserver.observe(element);
-  intersectionObserverList.push(intersectionObserver);
-}
-
-const intersectionObserverCallback = (index: number, entries: IntersectionObserverEntry[], store: CarouselStore) => {
-  entries.forEach((entry) => {
-    if (store.carouselItemList[index]) {
-      const last = entry.intersectionRatio === 1 && (index + 1) === store.totalItems;
-      store.carouselItemList[index].intersectionRatio = entry.intersectionRatio;
-      store.carouselItemList[index].fullyVisible = entry.intersectionRatio === 1;
-      store.carouselItemList[index].partiallyVisible = entry.intersectionRatio > 0 && entry.intersectionRatio < 1;
-      store.carouselItemList[index].notVisible = entry.intersectionRatio === 0;
-      store.carouselItemList[index].firstFullyVisible = entry.intersectionRatio === 1 && index === 0;
-      store.carouselItemList[index].lastFullyVisible = entry.intersectionRatio === 1 && last;
-      entry.target.setAttribute('aria-hidden', String(store.carouselItemList[index].fullyVisible === false));
-      entry.target.setAttribute('aria-current', String(store.carouselItemList[index].fullyVisible));
-      entry.target.setAttribute('aria-current', String(store.carouselItemList[index].fullyVisible));
-      entry.target.closest('.wrapper')?.querySelector('nav')?.children[index].setAttribute('aria-expanded', String(store.carouselItemList[index].fullyVisible));
-      store.scrolledToStart = store.carouselItemList.some((item) => item.firstFullyVisible);
-      store.scrolledToEnd = store.carouselItemList.some((item) => item.lastFullyVisible);
-    }
-
-
-
   });
-};
 
-const navigateDirection = (direction: 'previous' | 'next', ul: Element | undefined) => {
+  return (
+    <div ref={sentinel}>
+      <Slot></Slot>
+    </div>
+  );
+});
 
+const navigateDirection = (
+  direction: "previous" | "next",
+  ul: Element | undefined
+) => {
   if (ul) {
-    if (direction === 'previous') {
-      ul.scrollTo({ left: ul.scrollLeft - (ul.clientWidth / 2) })
+    if (direction === "previous") {
+      ul.scrollTo({ left: ul.scrollLeft - ul.clientWidth / 2 });
     } else {
-      ul.scrollTo({ left: ul.scrollLeft + (ul.clientWidth / 2) })
+      ul.scrollTo({ left: ul.scrollLeft + ul.clientWidth / 2 });
     }
-
   }
-
-}
+};
 
 const navigateToIndex = (index: number, ul: Element | undefined) => {
   if (ul) {
     const li = ul.children[index];
-    li.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' });
+    li.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
   }
-}
+};
 
-export interface CarouselStore {
-  'aria-label': string;
-  showTabNavigation: boolean;
-  scrolledToStart: boolean;
-  scrolledToEnd: boolean;
-  totalItems: number;
-  carouselItemList: CarouselItem[];
-}
-
-export interface CarouselItem {
-  index: number;
-  intersectionRatio: number;
-  fullyVisible: boolean;
-  firstFullyVisible: boolean;
-  lastFullyVisible: boolean;
-  partiallyVisible: boolean;
-  notVisible: boolean;
-  scrollPercentage: number;
-}
-
-
+// spotlight
+// set up examples
