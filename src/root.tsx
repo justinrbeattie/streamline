559dcb51e813f76cb416356ed2c2a5ad38a58615
Justin Beattie <jrb@fiftydegreesnorth.com>
2024-03-12 09:57:34 +1100

    compressed

--- src/root.tsx
+++ src/root.tsx
@@ -23,14 +23,6 @@ export default component$(() => {
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
         <ServiceWorkerRegister />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-      {/* 
-// @ts-ignore */}
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
-          rel="stylesheet"
-        />
       </head>
       <body lang="en">
         <RouterOutlet />
