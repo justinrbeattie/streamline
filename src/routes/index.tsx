a0c3f38ec2387675ad117b4103f812efe6bf5ca7
Justin Beattie <jrb@fiftydegreesnorth.com>
2024-03-14 18:44:18 +1100

    context baCK

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useContext } from "@builder.io/qwik";
 import { type DocumentHead } from "@builder.io/qwik-city";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import {
@@ -7,8 +7,8 @@ import {
   fetchOneEntry
 
 } from "@builder.io/sdk-qwik";
-/* import { CUSTOM_COMPONENTS } from "~/components/builder-registry";
-import { LayoutContext } from "./layout"; */
+import { CUSTOM_COMPONENTS } from "~/components/builder-registry";
+import { LayoutContext } from "./layout";
 
 export const BUILDER_MODEL = "page";
 
@@ -38,7 +38,7 @@ export const useBuilderContent = routeLoader$(async ({ url, error }) => {
 
 export default component$(() => {
   const content = useBuilderContent();
-/*   const layoutContext = useContext(LayoutContext); */
+  const layoutContext = useContext(LayoutContext);
 
 
 
@@ -48,7 +48,8 @@ export default component$(() => {
         model={BUILDER_MODEL}
         content={content.value}
         apiKey={import.meta.env.PUBLIC_BUILDER_API_KEY}
-
+        customComponents={CUSTOM_COMPONENTS}
+        context={layoutContext}
       />
     </div>
   );
@@ -63,3 +64,4 @@ export const head: DocumentHead = {
     },
   ],
 };
+
