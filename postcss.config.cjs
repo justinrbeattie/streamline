bb94b18c4cd6eb5d5942b282d2820ddc43fd9e3f
Justin Beattie <jrb@fiftydegreesnorth.com>
2024-03-16 12:23:37 +1100

    jit props

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,15 @@
+const postcssJitProps = require('postcss-jit-props');
+const path = require('path');
+
+
 module.exports = {
   plugins: {
     autoprefixer: {},
+    'postcss-jit-props': postcssJitProps({
+      files: [
+        path.resolve(__dirname, 'node_modules/open-props/open-props.min.css'),
+      ],
+    }),
     "postcss-preset-env": {
       stage: 3,
       features: {
--- src/components/design/main/main.tsx
+++ src/components/design/main/main.tsx
@@ -1,10 +1,8 @@
-import { component$, Slot, useContext, useStyles$ } from "@builder.io/qwik";
-import styles from "./main.css?inline";
+import { component$, Slot, useContext} from "@builder.io/qwik";
 import { LayoutContext } from "~/routes/layout";
 
 
 export const MainComponent = component$(() => {
-  useStyles$(styles);
   const layoutContext = useContext(LayoutContext);
   return (
     <main ref={layoutContext.mainRef}>
