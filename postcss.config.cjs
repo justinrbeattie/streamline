96b3c112b2d59d3113ff27bebdc62d4b2cf8ae4f
Justin Beattie <jrb@fiftydegreesnorth.com>
2024-03-16 18:50:42 +1100

    jit props

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,9 @@
-const postcssJitProps = require('postcss-jit-props');
 const OpenProps = require('open-props');
 
 
 module.exports = {
   plugins: {
     autoprefixer: {},
-    'postcss-jit-props':postcssJitProps(OpenProps),
     "postcss-preset-env": {
       stage: 3,
       features: {
@@ -13,6 +11,7 @@ module.exports = {
       },
     },
     "css-has-pseudo": {},
-    "@csstools/postcss-oklab-function":{}
+    "@csstools/postcss-oklab-function":{},
+    'postcss-jit-props': { ...OpenProps },
   },
 }
