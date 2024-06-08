cb1ed70b531d740c6a00c9f2e67bed62b85bdff8
Justin Beattie <jrb@fiftydegreesnorth.com>
2024-06-08 15:11:00 +1000

    update

--- src/components/layout/announcement-bar/announcement-bar.tsx
+++ src/components/layout/announcement-bar/announcement-bar.tsx
@@ -5,7 +5,7 @@ import styles from "./announcement-bar.css?inline";
 export const AnnouncementBar = component$(() => {
     useStylesScoped$(styles);
     return (
-        <div class="announcement-bar">
+        <div class="announcement-bar brand-primary">
             <h6>Announcement Bar</h6>
         </div>
     );
